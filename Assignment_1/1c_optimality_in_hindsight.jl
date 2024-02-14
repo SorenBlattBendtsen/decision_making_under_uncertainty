7a2e809bde71b8ca4031b156b88e88111477ea3e
SorenBlattBendtsen <s164521@student.dtu.dk>
2024-02-14 20:52:09 +0100

    1c working now :+1: :100:

--- Assignment_1/1c_optimality_in_hindsight.jl
+++ Assignment_1/1c_optimality_in_hindsight.jl
@@ -25,29 +25,28 @@ function Calculate_OiH_solution(price)
     @variable(model_1c, 0 <= x_wt[w in W, t in sim_T]) # Coffee ordered at warehouse w in period t
     @variable(model_1c, 0 <= z_wt[w in W, t in sim_T]) # Coffee stored at warehouse w in period t
     @variable(model_1c, 0 <= y_send_wqt[w in W, q in W, t in sim_T]) # Coffee sent from warehouse w to warehouse q in period t
-    @variable(model_1c, 0 <= y_receive_qwt[q in W, w in W, t in sim_T]) # Coffee received at warehouse w from warehouse q in period t
+    @variable(model_1c, 0 <= y_receive_wqt[w in W, q in W, t in sim_T]) # Coffee received at warehouse w from warehouse q in period t
     @variable(model_1c, 0 <= m_wt[w in W, t in sim_T]) # Missing demand at warehouse w in period t
 
     # Define the objective function
     @objective(model_1c, Min, sum(p_wt[w,t] * x_wt[w,t] for w in W, t in sim_T) 
                             + sum(cost_miss[w] * m_wt[w,t] for w in W, t in sim_T) 
-                            + (sum(cost_tr[w,q] * y_send_wqt[w,q,t] for w in W, q in W, t in sim_T)
-                            + sum(cost_tr[q,w] * y_receive_qwt[q,w,t] for q in W, w in W, t in sim_T))/2)
+                            + sum(cost_tr[w,q] * y_send_wqt[w,q,t] for w in W, q in W, t in sim_T))
 
     # Define the constraints
 
     # demand hour 1
-    @constraint(model_1c, demand_fulfillment_t0[w in W, q in W],
+    @constraint(model_1c, demand_fulfillment_t0[w in W],
                 x_wt[w,1] - z_wt[w,1] + initial_stock[w]
-                + sum(y_receive_qwt[q,w,1] for q in W)
-                - sum(y_send_wqt[w,q,1] for w in W)
+                + sum(y_receive_wqt[w,q,1] for q in W)
+                - sum(y_send_wqt[w,q,1] for q in W)
                 + m_wt[w,1] == demand[w,1])
 
     #demand hour t
-    @constraint(model_1c, demand_fulfillment[w in W, q in W, t in sim_T[2:end]],
+    @constraint(model_1c, demand_fulfillment[w in W, t in sim_T[2:end]],
                 x_wt[w,t] - z_wt[w,t] + z_wt[w,t-1]
-                + sum(y_receive_qwt[q,w,t] for q in W) 
-                - sum(y_send_wqt[w,q,t] for w in W)
+                + sum(y_receive_wqt[q,w,t] for q in W) 
+                - sum(y_send_wqt[w,q,t] for q in W)
                 + m_wt[w,t] == demand[w,t])
 
     # Storage capacity
@@ -58,12 +57,12 @@ function Calculate_OiH_solution(price)
     @constraint(model_1c, transportation_capacity[w in W, q in W, t in sim_T],
                 y_send_wqt[w,q,t] <= transport_capacities[w,q])
 
-    # # # constraint saying the amount sent from w to q is the same as received at q from w
+    # constraint saying the amount sent from w to q is the same as received at q from w
     @constraint(model_1c, send_receive[w in W, q in W, t in sim_T], 
-                 y_send_wqt[w,q,t] == y_receive_qwt[q,w,t])
+                 y_send_wqt[w,q,t] == y_receive_wqt[q,w,t])
 
 
-    #Storage before transfer
+    #Storage one day before transfer, initial stock in t=1
     @constraint(model_1c, storage_t1[w in W, q in W],
                 y_send_wqt[w,q,1] <= initial_stock[w])
 
@@ -85,7 +84,7 @@ function Calculate_OiH_solution(price)
                 for q in W
                     if q != w
                         @printf("Send to warehouse %i: %0.3f\n", q, value(y_send_wqt[w,q,t]))
-                        @printf("Receive from warehouse %i: %0.3f\n", q, value(y_receive_qwt[q,w,t]))
+                        @printf("Receive from warehouse %i: %0.3f\n", q, value(y_receive_wqt[w,q,t]))
                     end
                 end
             end
@@ -100,4 +99,3 @@ end
 
 # Run model for the given price
 Calculate_OiH_solution(p_wt)
-# comments: Might be working now. At least it is ordering at the right hours, but it is still not sending to other warehouses.
